f9dfab5343d3e71d6fd8b9feb0810d98762d0498
Ariyan Andryan Aryja <ariyanandryan@gmail.com>
2024-04-22 13:18:04 +0700

    update code

--- src/matematika/Matematika.java
+++ src/matematika/Matematika.java
@@ -1,45 +1,26 @@
 package matematika;
 
 public class Matematika {
-    int hasil;
+    //int hasil;
 
     //Method (mutator) Pertambahan
     void setPertambahan(int a, int b){
-        hasil = a + b;
+        System.out.println("Hasil " + a + " + " + b + " = " + (a + b));
     }
 
     //Method (mutator) Pengurangan
     void setPengurangan(int a, int b){
-        hasil = a - b;
+        //hasil = a - b;
+        System.out.println("Hasil " + a + " - " + b + " = " + (a - b));
     }
 
     //Method (mutator) Perkalian
     void setPerkalian(int a, int b){
-        hasil = a * b;
+        System.out.println("Hasil " + a + " * " + b + " = " + (a * b));
     }
 
     //Method (mutator) Pembagian
     void setPembagian(int a, int b){
-        hasil = a / b;
-    }
-
-    //Method (accessor) Pertambahan
-    int getPertambahan(){
-        return hasil;
-    }
-
-    //Method (accessor) Pengurangan
-    int getPengurangan(){
-        return hasil;
-    }
-
-    //Method (accessor) Perkalian
-    int getPerkalian(){
-        return hasil;
-    }
-
-    //Method (accessor) Pembagian
-    int getPembagian(){
-        return hasil;
+        System.out.println("Hasil " + a + " / " + b + " = " + (a / b));
     }
 }
--- src/matematika/MatematikaBeraksi.java
+++ src/matematika/MatematikaBeraksi.java
@@ -6,18 +6,14 @@ public class MatematikaBeraksi {
 
         //setPertambahan
         matematika.setPertambahan(2,3);
-        System.out.println("Hasil 2 + 3 = " + matematika.getPertambahan());
 
         //setPengurangan
         matematika.setPengurangan(3,2);
-        System.out.println("Hasil 3 - 2 = " + matematika.getPengurangan());
 
         //setPerkalian
         matematika.setPerkalian(4,5);
-        System.out.println("Hasil 4 * 5 = " + matematika.getPengurangan());
 
         //setPembagian
         matematika.setPembagian(8,4);
-        System.out.println("hasil 8 / 4 = " + matematika.getPembagian());
     }
 }
