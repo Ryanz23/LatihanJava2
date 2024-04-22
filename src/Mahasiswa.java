58649e0525f6e78aeea492251e1d06d1feaf36f3
Ariyan Andryan Aryja <ariyanandryan@gmail.com>
2024-04-22 11:49:44 +0700

    first commit

---
+++ src/Mahasiswa.java
@@ -0,0 +1,10 @@
+public class Mahasiswa {
+    String mengamati, meniru, modifikasi;
+
+    //Add Method
+    void printMahasiswa(){
+        System.out.println("Mahasiswa " + mengamati);
+        System.out.println("Mahasiswa " + meniru);
+        System.out.println("Mahasiswa " + modifikasi);
+    }
+}
---
+++ src/MahasiswaBeraksi.java
@@ -0,0 +1,15 @@
+public class MahasiswaBeraksi {
+    public static void main(){
+        //Create object
+        Mahasiswa mahasiswa = new Mahasiswa();
+
+        //Add attribute and value
+        mahasiswa.mengamati = "Mengamati Pelajaran";
+        mahasiswa.meniru = "Meniru Teman";
+        mahasiswa.modifikasi = "Modifikasi";
+
+        //Call Method
+        mahasiswa.printMahasiswa();
+
+    }
+}
