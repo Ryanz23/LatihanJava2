4161438bbe71925bdebc8a4881057564c9feb6a2
Ariyan Andryan Aryja <ariyanandryan@gmail.com>
2024-04-22 14:00:17 +0700

    update code

--- src/matematika/Matematika.java
+++ src/matematika/Matematika.java
@@ -1,26 +1,41 @@
 package matematika;
 
 public class Matematika {
-    //int hasil;
+    int hasil;
 
     //Method (mutator) Pertambahan
     void setPertambahan(int a, int b){
-        System.out.println("Hasil " + a + " + " + b + " = " + (a + b));
+        hasil = a + b;
+    }
+    //Method (accessor)
+    int getPertambahan(){
+        return hasil;
     }
 
     //Method (mutator) Pengurangan
     void setPengurangan(int a, int b){
-        //hasil = a - b;
-        System.out.println("Hasil " + a + " - " + b + " = " + (a - b));
+        hasil = a - b;
+    }
+    //Method (accessor)
+    int getPengurangan(){
+        return  hasil;
     }
 
     //Method (mutator) Perkalian
     void setPerkalian(int a, int b){
-        System.out.println("Hasil " + a + " * " + b + " = " + (a * b));
+        hasil = a * b;
+    }
+    //Method (accessor)
+    int getPerkalian(){
+        return hasil;
     }
 
     //Method (mutator) Pembagian
     void setPembagian(int a, int b){
-        System.out.println("Hasil " + a + " / " + b + " = " + (a / b));
+        hasil = a / b;
+    }
+    //Method (accessor)
+    int getPembagian(){
+        return hasil;
     }
 }
--- src/matematika/MatematikaBeraksi.java
+++ src/matematika/MatematikaBeraksi.java
@@ -1,19 +1,49 @@
 package matematika;
 
+import java.util.Scanner;
+
 public class MatematikaBeraksi {
     public static void main(String[]args){
         Matematika matematika = new Matematika();
+        Scanner scanner = new Scanner(System.in);
 
         //setPertambahan
-        matematika.setPertambahan(2,3);
+        //input variable
+        System.out.print("Input a : ");
+        int a = scanner.nextInt();
+        System.out.print("Input b : ");
+        int b = scanner.nextInt();
+
+        matematika.setPertambahan(a, b);
+        System.out.println("Hasil " + a + " + " + b + " = " + matematika.getPertambahan());
 
         //setPengurangan
-        matematika.setPengurangan(3,2);
+        //input variable
+        System.out.print("Input a : ");
+        a = scanner.nextInt();
+        System.out.print("Input b : ");
+        b = scanner.nextInt();
+
+        matematika.setPengurangan(a, b);
+        System.out.println("Hasil " + a + " - " + b + " = " + matematika.getPengurangan());
 
         //setPerkalian
-        matematika.setPerkalian(4,5);
+        //input variable
+        System.out.print("Input a : ");
+        a = scanner.nextInt();
+        System.out.print("Input b : ");
+        b = scanner.nextInt();
+
+        matematika.setPerkalian(a, b);
+        System.out.println("Hasil " + a + " * " + b + " = " + matematika.getPerkalian());
 
         //setPembagian
-        matematika.setPembagian(8,4);
+        System.out.print("Input a : ");
+        a = scanner.nextInt();
+        System.out.print("Input b : ");
+        b = scanner.nextInt();
+
+        matematika.setPembagian(a,b);
+        System.out.println("Hasil " + a + " / " + b + " = " + matematika.getPembagian());
     }
 }
