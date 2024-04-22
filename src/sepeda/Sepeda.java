f070ba410ece8510550eb2134a7a5e91306a815e
Ariyan Andryan Aryja <ariyanandryan@gmail.com>
2024-04-22 12:07:08 +0700

    add comment

--- src/sepeda/Sepeda.java
+++ src/sepeda/Sepeda.java
@@ -3,10 +3,12 @@ package sepeda;
 public class Sepeda {
     int gir;
 
+    //Method (mutator) with parameter
     void setGir(int pertambahanGir){
         gir = gir + pertambahanGir;
     }
 
+    //Method (accessor)
     int getGir(){
         return gir;
     }
--- src/sepeda/SepedaBeraksi.java
+++ src/sepeda/SepedaBeraksi.java
@@ -4,9 +4,11 @@ public class SepedaBeraksi {
     public static void main(String[]args){
         Sepeda sepedaku = new Sepeda();
 
+        //set gir value = 1
         sepedaku.setGir(1);
         System.out.println("Gir saat ini : " + sepedaku.getGir());
 
+        //adds 3 to the current gear position (1)
         sepedaku.setGir(3);
         System.out.println("Gir saat ini : " + sepedaku.getGir());
     }
