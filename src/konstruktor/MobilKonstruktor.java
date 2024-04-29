efe19e56e5f4e92885abdae83f6a897e5846b0d3
Ariyan Andryan Aryja <ariyanandryan@gmail.com>
2024-04-29 12:16:08 +0700

    add new file

---
+++ src/konstruktor/MobilKonstruktor.java
@@ -0,0 +1,8 @@
+package konstruktor;
+
+public class MobilKonstruktor {
+    public static void main(String[]args){
+        Mobil mobilku = new Mobil("Merah", 2003);
+        mobilku.info();
+    }
+}
