f65fa4b857648198fdf533461bd96d207e6403ee
Ariyan Andryan Aryja <ariyanandryan@gmail.com>
2024-04-29 12:17:05 +0700

    add new file

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -1,2 +1,25 @@
-package bank;public class Bank {
+package bank;
+
+public class Bank {
+    int saldo;
+
+    public Bank(int saldo){
+        this.saldo = saldo;
+    }
+    public void simpanUang(int jumlah){
+        saldo += jumlah;
+        System.out.println("Simpan Uang : Rp. " + jumlah);
+    }
+    public void ambilUang(int jumlah){
+        if (jumlah > saldo){
+            System.out.println("Saldo tidak mencukupi");
+        }else {
+            saldo -= jumlah;
+            System.out.println("Ambil uang : Rp. " + jumlah);
+            System.out.println("Saldo saat ini : Rp. " + saldo);
+        }
+    }
+    public void getSaldo(){
+        System.out.println("Saldo saat ini : Rp. " + saldo);
+    }
 }
--- src/bank/BankBeraksi.java
+++ src/bank/BankBeraksi.java
@@ -1,2 +1,18 @@
-package bank;public class BankBeraksi {
+package bank;
+
+import java.util.Scanner;
+
+public class BankBeraksi {
+    Bank bank = new Bank(1000000);
+
+    public void run(){
+        System.out.println("Selamat Datang di Bank BCA");
+        bank.getSaldo();
+        bank.simpanUang(100000);
+        bank.ambilUang(50000);
+    }
+    public static void main(String[]args){
+        BankBeraksi bank = new BankBeraksi();
+        bank.run();
+    }
 }
