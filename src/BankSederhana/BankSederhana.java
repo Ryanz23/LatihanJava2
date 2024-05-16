33bc933e6a2c6f18a62d995fff359a1a2257ec33
Ariyan Andryan Aryja <ariyanandryan@gmail.com>
2024-05-16 10:25:22 +0700

    update code

--- src/BankSederhana/BankSederhana.java
+++ src/BankSederhana/BankSederhana.java
@@ -18,30 +18,40 @@ public class BankSederhana {
         System.out.println("Saldo Anda Saat Ini adalah Rp " + saldo);
 
         //Input jumlah uang yang akan disimpan
-        System.out.println("Silakan isikan nominal yang anda akan simpan disini: ");
-        nominal = scanner.nextInt();
-        System.out.println("Terima Kasih, Saldo anda sekarang : Rp " + (saldo += nominal));
-
-        //Ambil uang
-        System.out.println("Apakah anda akan mengambil uang? (ya/tidak)");
+        System.out.println("Apakah anda akan menyimpan uang? (ya/tidak)");
         String jawaban = scanner.next();
 
-        //Jika ya maka output akan meminta untuk melakukan input terhadap jumlah nominal
+        //Jika ya maka program akan meminta untuk input nominal
         if (jawaban.equals("ya")) {
-            System.out.println("Silakan isikan nominal yang akan diambil disini: ");
+            System.out.println("Silakan isikan nominal yang anda akan simpan disini: ");
             nominal = scanner.nextInt();
 
-            //Jika nominal melebihi saldo di bank maka output akan langsung menutup program
-            if (nominal > saldo) {
-                System.out.println("Maaf saldo anda tidak cukup");
-                System.out.println("Terima Kasih, Saldo anda sekarang : Rp " + saldo);
-            }else {
-                System.out.println("Terima Kasih, Saldo anda sekarang : Rp " + (saldo -= nominal));
-            }
-        } else if (jawaban.equals("tidak")) {
-            //Jika tidak melakukan pengambilan uang output langsung menutup program
-            System.out.println("Terima Kasih, Saldo anda sekarang : Rp " + saldo);
-        }
+            System.out.println("Terima Kasih, Saldo anda sekarang : Rp " + (saldo += nominal));
 
+        //Jika tidak maka akan diarahkan apakah akan mengambil uang atau tidak
+        } else if (jawaban.equals("tidak")) {
+            System.out.println("Apakah anda akan mengambil uang? (ya/tidak)");
+            String jawaban2  = scanner.next();
+
+            //Jika ya maka output akan meminta untuk melakukan input terhadap jumlah nominal
+            if (jawaban2.equals("ya")) {
+                System.out.println("Silakan isikan nominal yang akan diambil disini: ");
+                nominal = scanner.nextInt();
+
+                //Jika nominal melebihi saldo di bank maka output akan langsung menutup program
+                if (nominal > saldo) {
+                    System.out.println("Maaf saldo anda tidak cukup");
+                    System.out.println("Terima Kasih, Saldo anda sekarang : Rp " + saldo);
+
+                //Nominal kurang dari atau sama dengan saldo di bank
+                }else if (nominal <= saldo){
+                    System.out.println("Terima Kasih, Saldo anda sekarang : Rp " + (saldo -= nominal));
+                }
+
+            //Jika tidak maka program akan langsung selesai
+            } else if (jawaban2.equals("tidak")) {
+                System.out.println("Terima Kasih, Saldo anda sekarang : Rp " + saldo);
+            }
+        }
     }
 }
