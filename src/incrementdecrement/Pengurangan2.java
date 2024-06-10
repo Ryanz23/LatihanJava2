1d5ef0e6d8df89eb9765325c71e73c6a7279cda1
Ariyan Andryan Aryja <ariyanandryan@gmail.com>
2024-06-10 12:36:17 +0700

    add new file

---
+++ src/incrementdecrement/Pengurangan2.java
@@ -0,0 +1,15 @@
+package incrementdecrement;
+
+public class Pengurangan2 {
+    public static void main(String[] args) {
+        int w, x, y, z;
+        w = 5;
+        x = 5;
+        y = 8 - x--;
+        System.out.println(w);
+        System.out.println(x);
+        System.out.println(y);
+        z = 8 - --w;
+        System.out.println(z);
+    }
+}
